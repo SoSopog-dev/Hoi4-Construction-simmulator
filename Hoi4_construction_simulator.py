efe8eb95ffb6a8556cbab46dbf7678a1c4ad912f
SoSopog-dev <84405494+SoSopog-dev@users.noreply.github.com>
2024-07-29 23:14:52 +0200

    Update Hoi4_construction_simulator.py
    improved now calculates bonus of whats beeing created

--- Hoi4_construction_simulator.py
+++ Hoi4_construction_simulator.py
@@ -1,15 +1,17 @@
 #Hoi4 construction simulator
 import math
-
+import csv
 
 #TODO
 """
-Change order to dict not list
-GUI?
-Show stats
+
+
+
 Times events
 Calculate Bonus
+make Consumer goods affected by stability
 """
+
 
 
 
@@ -21,12 +23,54 @@ class state():
         self.infra = infra
         self.slots = slots
 
-def calculate_bonus():
-    pass
+def eco_laws(law_name):
+    #TODO make other laws and return based on law_name
+    civilian_eco = {
+        "name" : "civilian_eco",
+        "consumer_goods" : 35/100,
+        "mills" : -30,
+        "civs" : -30,
+    }
+
+    return civilian_eco
+
+
+def calculate_bonus(natinal_spirits, advisors, construction_techs, trade_law, eco_law, building):
+    bonus = 0
+
+    #National Spirits
+    for spirit in natinal_spirits:
+        if building in spirit:
+            bonus += spirit[building]
+
+        if "construction" in spirit:
+            bonus += spirit["construction"]
+
+    #Advisors
+    for advisor in advisors:
+        if building in advisor:
+            bonus += advisor[building]
+
+        if "construction" in advisor:
+            bonus += advisor["construction"]
+
+    #Construction techs
+
+    for tech in  construction_techs:
+        bonus += tech
+    #Trade Law
+
+    bonus += trade_law["construction"]
+    #Eco Law
+
+    if building in eco_law:
+        bonus += eco_law[building]
+
+    return bonus
 
 def calculate_progress(state, construction_progress, available_civs, bonus):
     #print(available_civs)
-    construction_progress += 8 * available_civs * (1 + (state.infra/ 5)) * (1 + bonus/100) #NB!!! the base is normaly 5 not 8 
+    construction_progress += 5 * available_civs * (1 + (state.infra/ 5)) * (1 + bonus/100) #NB!!! the base is normaly 5 not 8 
 
     #print(f"This is the base contruction:{8 * available_civs * (1 + (state.infra / 5)) * (1 + bonus/100)}, that is 8 * {available_civs} = {8 * available_civs} \n * {(1+ (state.infra / 5))} * {1 + bonus/100}")
 
@@ -39,6 +83,15 @@ def get_civ_amount(states):
         civs += state.civs
     
     return civs
+
+def get_factory_amount(states):
+    factories = 0
+
+    for state in states:
+        factories += state.civs
+        factories += state.mills
+    
+    return factories
 
 def track_stats(states, stats):
     #STATES AND STATS MUST NOT BE CONFUSED
@@ -65,29 +118,64 @@ def make_task(state, construction_type, amount, construction_progress):
 
     return task
 
-def display_stats():
-    pass
+def save_stats(times, stats):
+    filename = 'data.csv'
 
-def innit():
+    # Open the file in write mode
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        
+        # Write the header
+        writer.writerow(['Column1', 'Column2'])
+        
+        # Write the data from the arrays
+        for item1, item2 in zip(times, stats):
+            writer.writerow([item1, item2])
+
+
+def init():
+
+    # --- States ---
     
     states = []
-    moscow = state(5, 2, 3, "Moscow", 15)
-    leningrad = state(20, 0, 4, "Leningrad", 25)
+    york = state(0, 0, 3, "York", 15)
+    dover = state(0, 0, 3, "Dover", 25)
+    rio = state(0, 0, 3, "Rio", 25)
+    london = state(33, 8, 5, "London", 30)
 
-    states.append(moscow)
-    states.append(leningrad) 
+    states.append(york)
+    states.append(dover) 
+    states.append(rio)
+    states.append(london)
 
 
 
-    queue = [make_task(moscow, "civs", 10,0),
-             make_task(moscow, "infra", 2,100),
-             make_task(leningrad, "civs", 10,0),
-             make_task(leningrad, "infra", 1,50)
+    queue = [   make_task(york, "infra", 1,0),
+                make_task(york, "civs", 3,0),
+                make_task(dover, "infra", 1,0),
+                make_task(dover, "civs", 3,0),
+                make_task(rio, "infra", 1,0),
+                make_task(rio, "civs", 3,0)
              ]
     
-    bonus = -20
+    #national spirits
 
-    CONSUMER_GOODS = 35/100
+    national_spirits = []
+    advisors = []
+
+    trade_law = {
+        "research" : 5,
+        "construction" : 10
+    }
+
+    eco_law = eco_laws("civilian_eco")
+
+    consumer_goods = eco_law["consumer_goods"] #TODO make stability affect consumergoods
+
+
+    #Constants
+
+
     CIV_COST = 10800
     MILL_COST = 7200
     INFRA_COST = 6000
@@ -100,11 +188,12 @@ def innit():
 
 
     time = 0
+    times = []
     stats = []
 
-    return states, queue, bonus, CONSUMER_GOODS, CIV_COST, MILL_COST, INFRA_COST, construction_types, stats, time
+    return states, queue, consumer_goods, CIV_COST, MILL_COST, INFRA_COST, national_spirits, advisors, trade_law, eco_law, construction_types, stats, time, times
 
-def main(states, queue, bonus, CONSUMER_GOODS, CIV_COST, MILL_COST, INFRA_COST, construction_types, stats, time):
+def main(states, queue, consumer_goods, CIV_COST, MILL_COST, INFRA_COST, national_spirits, advisors, trade_law, eco_law, construction_types, stats, time, times):
 
     """
     run = True
@@ -112,8 +201,11 @@ def main(states, queue, bonus, CONSUMER_GOODS, CIV_COST, MILL_COST, INFRA_COST, 
     """
     #TODO: calculate available civs 
     total_civs = get_civ_amount(states)
+    total_factories = get_factory_amount(states)
     #print(total_civs)
-    available_civs = math.floor(total_civs * (1-CONSUMER_GOODS))
+    available_civs =  total_civs - math.floor(total_factories * consumer_goods)
+
+    construction_techs = []
 
     if queue != []:
 
@@ -132,14 +224,15 @@ def main(states, queue, bonus, CONSUMER_GOODS, CIV_COST, MILL_COST, INFRA_COST, 
                 civs = available_civs
                 available_civs = 0
 
+            bonus = calculate_bonus(national_spirits, advisors, construction_techs, trade_law, eco_law, construction_type)
+
 
             construction_progress = calculate_progress(state, construction_progress, civs, bonus)
 
             
-
             
 
-            print(f"The contruction progress is at  {construction_progress} out of {CIV_COST}")
+            #print(f"The contruction progress is at  {construction_progress} out of {CIV_COST}")
 
             
 
@@ -163,16 +256,17 @@ def main(states, queue, bonus, CONSUMER_GOODS, CIV_COST, MILL_COST, INFRA_COST, 
             task["construction_progress"] = construction_progress
             task["amount"] = amount
     
-    else:
+    #else:
         #print(f"Done in {time} days, which is {(time / 30)} months, which is {(time) / 365.24} years")
-        i = input()
+        #i = input()
 
 
-    
+    times.append(time)
     time += 1
+    
     stats = track_stats(states, stats)
 
-    return time, stats, states, queue
+    return times, time, stats, states, queue
 
 
 
